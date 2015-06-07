1d4a7372719b45a25ba242323d1b3db1aff8b677
Abner Yang <abtyang@gmail.com>
2015-06-07 15:46:31 -0400

    delete flashcard functionality

--- shuffler/app/controllers/flashcards_controller.rb
+++ shuffler/app/controllers/flashcards_controller.rb
@@ -1,15 +1,6 @@
 class FlashcardsController < ApplicationController
 	protect_from_forgery with: :null_session
 
-	def new
-		@flashcard = Flashcard.new
-	end
-
-	def create 
-		Flashcard.create(flashcard_params)
-		redirect_to(user_flashcards_path)
-	end
-
 	def index
 		@user = User.find(session[:user_id])
 		@flashcards = @user.flashcards
@@ -21,6 +12,18 @@ class FlashcardsController < ApplicationController
 		end
 	end
 
+	def new
+		@user_id = User.find(session[:user_id])
+		@flashcard = Flashcard.new
+
+	end
+
+	def create 
+		Flashcard.create(flashcard_params)
+		redirect_to(user_flashcards_path)
+	end
+
+
 	def show
 		@flashcard = Flashcard.find(params[:id])
 		if @flashcard
@@ -31,6 +34,11 @@ class FlashcardsController < ApplicationController
 		end
 	end
 
+	def destroy
+		Flashcard.find(params[:id]).destroy
+		redirect_to(user_flashcards_path)
+	end
+	
 	private
 	def flashcard_params
 		params.require(:flashcard).permit(:chinese, :pinyin, :defin, :user_id)
