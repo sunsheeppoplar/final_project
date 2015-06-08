1bc8f6ef8d90122960f409bd2927d016cca64413
Abner Yang <abtyang@gmail.com>
2015-06-07 20:39:54 -0400

    flashcard show flip functionality

---
+++ shuffler/app/assets/javascripts/shuffler.js
@@ -0,0 +1,15 @@
+// JQuery that allows user to display whatever they're testing themselves on
+$(document).ready(function() {
+	$(document).on('click', '.action', function() {
+		var $this = $(this);
+		var target = $this.data('content');
+		$('div.action').not($this).each(function() {
+			var $other = $(this);
+			var otherTarget = $other.data('content');
+			$(otherTarget).hide();
+		});
+
+		$(target).show();
+
+	});
+});
